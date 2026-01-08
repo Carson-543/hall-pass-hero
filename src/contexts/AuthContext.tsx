31e08bf51e0ff0283bb2e54dcec3220e009003c3
Carson-543 <Carsoncorbin81@gmail.com>
2026-01-08 11:38:05 -0500

    Update AuthContext.tsx

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,7 +1,7 @@
 import { motion } from 'framer-motion';
 import { Button } from '@/components/ui/button';
 import { LogOut } from 'lucide-react';
-import { useAuth } from '@/hooks/use-auth'; // Double-check this filename is exactly use-auth.tsx
+import { useAuth } from '@/contexts/AuthContext'; // Double-check this filename is exactly use-auth.tsx
 import { useNavigate } from 'react-router-dom';
 
 export const TeacherHeader = () => {
